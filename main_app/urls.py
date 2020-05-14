c50206b512422f902403204b87aa49ccc0fe0447
freshta <freshta.bashari@gmail.com>
2020-05-14 16:02:25 -0700

    added the path and view for nv.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('nav/', views.nav, name='nav'),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def nav(request):
+  return render(request, 'nav.html')    
+
