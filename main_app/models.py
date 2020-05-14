82fdc77bb130ebc21c00d460c9c814025d47b55e
gdukes707 <Gdukes707@gmail.com>
2020-05-14 12:08:39 -0700

    profile and movie models added

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
+
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=150)
+    api_id = models.IntegerField()
+
+
+
+class Profile(models.Model):
+    display_name = models.CharField(max_length=50)
+    movies_list = models.ManyToManyField(Movie)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.display_name
+
+
