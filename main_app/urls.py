dfcaede23ec1df43af7169d84be1d1c06d2c0830
freshta <freshta.bashari@gmail.com>
2020-05-15 10:13:00 -0700

    working on edit page

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('accounts/profile/<int:profile_id>', views.profile, name = 'profile'),
     path('accounts/profile/<int:profile_id>/add_photo', views.add_photo, name = 'add_photo'),
     path('accounts/profile/<int:profile_id>/comment/', views.add_comment_to_movie, name='add_comment_to_movie'),
-    path('accounts/signup', views.signup, name='signup')
+    path('accounts/signup', views.signup, name='signup'),
+    path('accounts/profile/<int:profile_id>/edit', views.edit_profile, name='edit_profile'),
 ]
