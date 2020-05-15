ce38dc23eed59f4a6d1fef638d4fe16372819159
gdukes707 <Gdukes707@gmail.com>
2020-05-15 10:04:49 -0700

    adds new profile page

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,9 +1,14 @@
 from django import forms
-from .models import Comment
+from .models import Comment, Profile
 
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('author', 'text')
+        fields = ('text',)
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('display_name', 'email', 'first_name', 'last_name')
