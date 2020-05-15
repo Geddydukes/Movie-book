87d5206bc4230fa73d1a0d65f8e47387ea0d9430
freshta <freshta.bashari@gmail.com>
2020-05-15 12:30:23 -0700

    added the login redirect in setting

--- movie_book_project/settings.py
+++ movie_book_project/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-LOGIN_REDIRECT_URL = '/profile/'
+# login redirect
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
