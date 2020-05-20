2f5d48a96a5926898ff585de7f3fd90f26c1aadc
gdukes707 <Gdukes707@gmail.com>
2020-05-20 11:09:46 -0700

    new comment

--- movie_book_project/wsgi.py
+++ movie_book_project/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_book_project.settings')
 
 application = get_wsgi_application()
+
+
+# comment time
